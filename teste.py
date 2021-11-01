212717c4aba2695ba22d0263b27e3f1c2f360d0a
Aluno da Infinity School <aluno@3D.local>
2021-11-01 20:31:19 -0300

    Hello world!

---
+++ teste.py
@@ -0,0 +1,28 @@
+# x = 10
+# y = -2
+
+# if not x > 5 or y < 0:
+#     print("Funcionou!")
+
+
+
+
+# temperatura = int(input("Informe sua temperatura:"))
+
+# if temperatura > 37:
+#     print("Proibir a entrada!")
+# else:
+#     print ("Permitido a entrada!")
+
+
+
+a = 10
+b = 20
+
+aux = a
+print(aux)
+a = b
+print(a)
+b = aux
+
+print(a,b)
